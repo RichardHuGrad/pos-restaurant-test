3c8aed371abdd077d951a1291dd9b70db0005a13
PC-201806151734\Administrator <zn790909369@163.com>
2018-08-15 14:10:37 +0800

    将菜品分类

--- app/Controller/Component/PrintComponent.php
+++ app/Controller/Component/PrintComponent.php
@@ -129,7 +129,6 @@ class PrintComponent extends Component {
         }
         // according to order_id
         // find all items in order, print all items which is not print
-
         $order_id = $args['order_id'];
 
         $orderDetail = $this->Order->find('first', array(
@@ -147,47 +146,38 @@ class PrintComponent extends Component {
         $table = $orderDetail['Order']['table_no'];
         $order_no = $orderDetail['Order']['order_no'];
         $phone = $orderDetail['Order']['phone'];
-
-
         // get all unprinted items
         $printItems = $this->OrderItem->getUnprintItemsByOrderId($order_id);
-
-        
-
         if (!empty($printItems['K'])) {
-
-
-            // $printerName = $this->PrintPage->getKitchenPrinterName($args['restaurant_id']);//多查询一个打印机id
-
-        
+            // $printerName = $this->PrintPage->getKitchenPrinterName($args['restaurant_id']);//多查询一个打印机id        
             $print = new PrintLib();
  
             foreach($printItems['K'] as $item){
               foreach($item as $key=>$value){
-                $item[$key]["type"]=$this->Cousines->getPrinterId($value['item_id']);//查询菜品设定打印机
-
-                $type.=$item[$key]["type"].",";
-              }
-              print_r($type);
-                if($type['Cousines']['printer']!=""){
-
-                  $id=explode(",",$type["Cousines"]['printer']);
-                  foreach($id as $printerid){
+                $printerid1=$this->Cousines->getPrinterId($value['item_id']);//查询菜品设定打印机          
+                $item[$key]["type"]=$printerid1['Cousines']['printer'];
+                print_r($item);
+                if($item[$key]["type"]!=""){
+                  $id=explode(",",$item[$key]["type"]);
+                  foreach($id as $k=>$printerid){
+                    $food="";
+                    if($printerid == $item[$key]['type']){
+                      $food[$k]=$item[$key];
+                    }
+                    // $key=array_keys($item,$printerid);
+                    // print_R($key);
                     $printerName1=$this->PrintPage->getPrintName($printerid);
                     $printerName=$printerName1["PrintPage"]['name'];
-                    $print->printKitchenItemDoc($order_no, $table, $type, $printerName , $item,true, false,$phone);
+                    $print->printKitchenItemDoc($order_no, $table, $type, $printerName , $food,true, false,$phone);
                   }
                 }else{
                     $printerName1=$this->PrintPage->getPrintName(2);
                     $printerName=$printerName1["PrintPage"]['name'];
                     $print->printKitchenItemDoc($order_no, $table, $type, $printerName , $item,true, false,$phone);
-                }
-                
-             
+                }        
+              }    
             }
-
-        }
-
+          }
         if (!empty($printItems['C'])) {
             $printerName = $this->PrintPage->getServicePrinterName($args['restaurant_id']);
             // return $printerName;
@@ -196,7 +186,6 @@ class PrintComponent extends Component {
                 $print->printKitchenItemDoc($order_no, $table, $type, $printerName, $item, true, false);
             }
         }
-
         $this->OrderItem->setAllItemsToPrinted($order_id);
     }
 
@@ -374,36 +363,39 @@ class PrintComponent extends Component {
            foreach($cancel_items['K'] as $items){
                 for($i=0;$i<count($items);$i++){
 
-                   $type=$this->Cousines->getPrinterId($items[$i]['item_id']);//查询菜品设定打印机
-                   }
-                  // print_r($type);exit;
-                  if($type['Cousines']['printer']!=""){
-                      $id=explode(",",$type["Cousines"]['printer']);
-                      foreach($id as $printerid){
+                   $type1=$this->Cousines->getPrinterId($items[$i]['item_id']);//查询菜品设定打印机
+                   $items[$i]["type"]=$type1['Cousines']['printer'];
+                
+                // print_r($items);
+                   // $type=implode(",",$item);
+                  if($items[$i]["type"]!=""){
+                      $id=explode(",",trim($items[$i]["type"],","));
+                      foreach($id as $k=>$printerid){
+                       $food="";
                         $printerName1=$this->PrintPage->getPrintName($printerid);
+
                         $printerName=$printerName1["PrintPage"]['name'];
+                        if($printerid==$items[$i]["type"]){
+                          $food[$k]=$items[$i];
+                        }
                   // print_r($printerName);
-                        $debug_str = $print->printCancelledItems($order_no, $table, $type, $printerName, $items,true, false);
-                        
+                       $debug_str = $print->printCancelledItems($order_no, $table, $type, $printerName, $food,true, false);
                       }
                   }else{
                       $printerName1=$this->PrintPage->getPrintName(2);
                       $printerName=$printerName1["PrintPage"]['name'];
                       $debug_str = $print->printCancelledItems($order_no, $table, $type, $printerName, $items,true, false);
                   }
-                
-              // $debug_str = $print->printCancelledItems($order_no, $table, $type, $printerName, $items,true, false);
-            }
-          
-           
-            
+                }
+              }    
         }
+  
         if (!empty($cancel_items['C'])) {
             $printerName = $this->PrintPage->getServicePrinterName($args['restaurant_id']);
             $print = new PrintLib();
             
             foreach($cancel_items['C'] as $items){
-            	// $print->printCancelledItems($order_no, $table, $type, $printerName, $items,true, false);
+            	$print->printCancelledItems($order_no, $table, $type, $printerName, $items,true, false);
             }            
         }
 
